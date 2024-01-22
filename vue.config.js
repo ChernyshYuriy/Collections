e83be3e257427c8e23aedd43c9292dccadb3edab
Chernysh Yuriy <yuriy.chernysh97@gmail.com>
2024-01-22 11:42:24 +0200

    vue change for PWA

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
     assetsVersion: "0.4.0",
-    manifestCrossorigin: "./public/manifest.json",
+    // manifestCrossorigin: "./public/manifest.json",
     // configure the workbox plugin
     // workboxPluginMode: "InjectManifest",
     workboxOptions: {
