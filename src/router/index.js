f6747e2c2fbbcf667d30c08353a044322b1d3a42
Chernysh Yuriy <yuriy.chernysh97@gmail.com>
2024-01-28 10:08:40 +0200

    fix error in router

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,6 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-
-    component: function () {
-      return import("../views/AboutView.vue");
-    },
-  },
-  {
     path: "/login",
     name: "login",
 
